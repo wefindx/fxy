89a91fc3eb921331d57c8197a64564ebe5ced588
Mindey <mindey@qq.com>
2018-10-13 01:28:04 +0000

    upd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fxy',
-    version='0.1.1',
+    version='0.1.2',
     description='Scientific functions.',
     url='https://github.com/mindey/fxy',
     author='Mindey',
