a3be5b0399f11b2877faf78157f32d5a44f571e6
Mindey <mindey@qq.com>
2021-03-07 17:17:23 +0000

    update: make sympy required, cause it's pure python

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='fxy',
-    version='0.3.0',
+    version='0.3.1',
     description='Convenience imports and scientific functions.',
     long_description=long_description,
     url='https://github.com/mindey/fxy',
@@ -13,7 +13,7 @@ setup(
     author_email='mindey@qq.com',
     license='UNLICENSE',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires=['mpmath', 'bpython', 'ipython'],
+    install_requires=['mpmath', 'bpython', 'ipython', 'sympy'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
         'shells': ['bpython', 'ipython'],
