f6b87ee8e2001ff7de08d2e10276c80026719920
Mindey <mindey@mindey.com>
2024-10-16 16:00:50 +0000

    add jupyter_client dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='fxy',
-    version='0.7.0',
+    version='0.7.1',
     description='Convenience imports and scientific functions.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
@@ -14,7 +14,7 @@ setup(
     author_email='mindey@mindey.com',
     license='UNLICENSE',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires=['mpmath', 'ipython', 'sympy', 'xarray', 'matplotlib', 'scikit-learn', 'scipy', 'statsmodels', 'seaborn', 'PyQt5', 'windows-curses >= 2.2.0 ; platform_system=="Windows"', 'eday'],
+    install_requires=['mpmath', 'ipython', 'sympy', 'xarray', 'matplotlib', 'scikit-learn', 'scipy', 'statsmodels', 'seaborn', 'PyQt5', 'windows-curses >= 2.2.0 ; platform_system=="Windows"', 'eday', 'jupyter_client'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
